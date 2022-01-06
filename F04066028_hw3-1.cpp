d23f9318cf02bd0413886699e5dadfd6dfacf794
tzuyichan <F04066028@gs.ncku.edu.tw>
2022-01-07 03:57:43 +0800

    Add `print_preorder()`

--- F04066028_hw3-1.cpp
+++ F04066028_hw3-1.cpp
@@ -10,6 +10,7 @@ using namespace std;
 vector<int> load_binary_tree(ifstream &);
 void print(const vector<int> &);
 int search(const int &x, const vector<int> &);
+void print_preorder(const vector<int> &, const vector<int> &);
 
 int main(void)
 {
@@ -19,7 +20,7 @@ int main(void)
     cin >> filename;
 
     // Read file.
-    ifstream inFile(filename, ios::in);
+    ifstream inFile("./testcases/3-1/" + filename, ios::in);
     if (!inFile)
     {
         cerr << "Failed to open " << filename << "\n";
@@ -35,14 +36,17 @@ int main(void)
         // get input file contents
         string order; // problem type
         getline(inFile, order);
-        cout << order << "\n";
         getline(inFile, line);
         int n_nodes = stoi(line); // number of nodes
-        cout << n_nodes << "\n";
         vector<int> porder = load_binary_tree(inFile);
         vector<int> inorder = load_binary_tree(inFile);
-        print(porder);
-        print(inorder);
+
+        // print order conversion
+        if (order == "postorder-inorder")
+        {
+            print_preorder(porder, inorder);
+            cout << "\n";
+        }
     }
 
     return 0;
@@ -56,6 +60,28 @@ int search(const int &x, const vector<int> &tree)
             return i;
     }
     return -1;
+}
+
+void print_preorder(const vector<int> &post, const vector<int> &in)
+{
+    int root_idx = search(post.back(), in);
+    int rsubtree_len = post.size() - root_idx - 1;
+
+    cout << post.back() << " ";
+
+    if (root_idx != 0)
+    {
+        vector<int> sub_post(post.begin(), post.end() - rsubtree_len - 1);
+        vector<int> sub_in(in.begin(), in.begin() + root_idx);
+        print_preorder(sub_post, sub_in);
+    }
+
+    if (root_idx != in.size() - 1)
+    {
+        vector<int> sub_post(post.end() - rsubtree_len - 1, post.end() - 1);
+        vector<int> sub_in(in.begin() + root_idx + 1, in.end());
+        print_preorder(sub_post, sub_in);
+    }
 }
 
 vector<int> load_binary_tree(ifstream &inFile)
